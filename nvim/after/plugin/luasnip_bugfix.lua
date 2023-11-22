20312a40822ee584af8616ec57c279b030e59273
Shamim <abir09bs@gmail.com>
2023-11-22 20:53:10 +0100

    bugfix: weird tab line jump

---
+++ nvim/after/plugin/luasnip_bugfix.lua
@@ -0,0 +1,14 @@
+-- https://github.com/L3MON4D3/LuaSnip/issues/258
+-- stop weird luasnip-caused line jump when tabbing.
+
+vim.api.nvim_create_autocmd('ModeChanged', {
+  pattern = '*',
+  callback = function()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end
+})
