ed88e6e838f9f1e8a9cdb2079ce5053cf758e3c6
Shamim <abir09bs@gmail.com>
2024-01-30 03:47:42 +0100

    update stow.sh

--- stow.sh
+++ stow.sh
@@ -9,7 +9,11 @@ if [ "$1" = "-c" ]; then
 fi
 
 echo "[status] running gnu stow..."
-stow --restow --target=$HOME xorg zsh \
+
+stow --restow --target=$HOME xorg \
+	2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2) # bugfix
+
+stow --restow --target=$HOME zsh \
 	2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2) # bugfix
 
 mkdir -p $HOME/.scripts
